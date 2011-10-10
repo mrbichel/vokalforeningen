b5acab4357e9f9318c9d24cf37952cf68ed505b3
vokal <vokal@tango.johan.cc>
2011-10-10 11:12:23 +0200

    removed settings_local.py

--- settings_local.py
+++
@@ -1,9 +0,0 @@
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'b-6!1u=#4afbgasbgaivpopopoiynhjs64kctag6e_!2_iw'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test.db',                      # Or path to database file if using sqlite3.
-    }
-}
